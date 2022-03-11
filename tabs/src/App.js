0c1bcbc1f6f1246b94a4ddce05901fecc9ec931b
Susan <szhou089@gmail.com>
2022-03-10 18:52:58 -0800

    "Add state prop and css"

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -4,7 +4,11 @@ import Tabs from './componenets/Tabs';
 function App() {
   return (
     <div className="App">
-      <Tabs/>
+      <Tabs tabsItems={[
+        {label: "Tab1", content:"Tab1 content is showing here"},
+        {label: "Tab2", content:"Tab2 content is showing here"},
+        {label: "Tab3", content:"Tab3 content is showing here"}
+      ]}/>
     </div>
   );
 }
--- tabs/src/componenets/Tabs.jsx
+++ tabs/src/componenets/Tabs.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useState} from "react";
 
 const Tabs = (props) => {
-    return <div>Tabs</div>
-}
+    console.log(props);
+    //use selected index to keep track which tab is selected
+    const [selectedIndex, setSelectedIndex] = useState(1);
+    return (
+        <div>
+            <div style={{display: "flex", marginTop:40, marginLeft: 500}}>
+                {
+                    props.tabsItems.map((tab, i)=> {
+                        return (
+                            <div key={i} onClick={(event) => {setSelectedIndex(i);}} style={{padding:20, marginRight:10, border: "1px solid grey"}}>{tab.label}</div>
+                        );
+                    })
+                }
+            </div>
+            <div style={{width: 600, height: 300, marginLeft: 300, marginTop: 40, border: "1px solid grey"}}>
+                <h2>{props.tabsItems[selectedIndex].content}</h2>
+            </div>
+        </div>    
+    );    
+};
 
 export default Tabs;
